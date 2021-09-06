1c959b3a0ae10ea53a3b8fd6c995e7a86474ad7a
Krish <vamsivamzy@gmail.com>
2021-09-06 16:31:32 +0530

    modified code

--- test1.py
+++ test1.py
@@ -1,7 +1,6 @@
-
 num_list = ['69','45','33','96','75','27','39','59','65','12']
 num_list.sort()
-print(f'the sorted list is {num_list}')
+print(f"the sorted order is", num_list)
 
 print(f"If you see a sorted list above then the execution is done perfectly!")
 
