ed8c9417f9d944fb14c2a61cf4250d1862928e2f
Krish <vamsivamzy@gmail.com>
2021-09-06 16:39:27 +0530

    modified code

--- test1.py
+++ test1.py
@@ -2,9 +2,4 @@ num_list = ['69','45','33','96','75','27','39','59','65','12']
 num_list.sort()
 print(num_list)
 
-print(f"If you see a sorted list above then the execution is done perfectly!")
-
-
-
-
 
